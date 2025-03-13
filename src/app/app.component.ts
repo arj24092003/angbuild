51465d744cdbf3362c8bb3ae81f0889bde3222bd
arj24092003 <144003117+arj24092003@users.noreply.github.com>
2025-03-13 15:32:01 +0530

    Create app.component.ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Hello, Angular!</h1>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'simple-angular-project';
+}
